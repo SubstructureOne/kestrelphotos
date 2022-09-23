ca5b7cdd8724c18019583092415d8ab2df4b9a7a
dek <dek@substructure.one>
2022-09-22 23:51:17 -0400

    Remove footer until updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,19 @@
 const Footer = () => {
     return <div className="footer wf-section">
-        <div className="w-container">
-            <div>
-                <a href="#" className="social-icon-link w-inline-block">
-                    <img src="images/social-03.svg" alt="" width={20} />
-                </a>
-                <a href="#" className="social-icon-link w-inline-block">
-                    <img src="images/social-18.svg" alt="" width={20} />
-                </a>
-                <a href="#" className="social-icon-link w-inline-block">
-                    <img src="images/social-30.svg" alt="" width={20} />
-                </a>
-            </div>
-        </div>
-        <div className="footer-text">Powered by Webflow</div>
+        {/*<div className="w-container">*/}
+        {/*    <div>*/}
+        {/*        <a href="#" className="social-icon-link w-inline-block">*/}
+        {/*            <img src="images/social-03.svg" alt="" width={20} />*/}
+        {/*        </a>*/}
+        {/*        <a href="#" className="social-icon-link w-inline-block">*/}
+        {/*            <img src="images/social-18.svg" alt="" width={20} />*/}
+        {/*        </a>*/}
+        {/*        <a href="#" className="social-icon-link w-inline-block">*/}
+        {/*            <img src="images/social-30.svg" alt="" width={20} />*/}
+        {/*        </a>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
+        {/*<div className="footer-text">Powered by Webflow</div>*/}
     </div>
 }
 
